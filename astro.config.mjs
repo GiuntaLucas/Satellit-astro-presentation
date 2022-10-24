c9f04c23d4f76647e49159445ac4b0a948ba114e
GiuntaLucas <lucas-giunta@outlook.com>
2022-10-24 21:04:42 +0200

    ssr integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   integrations: [svelte()]
 });
