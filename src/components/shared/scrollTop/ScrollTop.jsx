94e2d9a64a09ce7bfc2a5eebc951434d1d883851
GiuntaLucas <lucas-giunta@outlook.com>
2022-11-11 11:10:16 +0100

    Header astro => React

--- src/components/shared/scrollTop/ScrollTop.jsx
+++ src/components/shared/scrollTop/ScrollTop.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from "react"
+import {useEffect, useState} from "react";
 import './ScrollTop.css';
 
-export const ScrollTop = () => {
+export default function ScrollTop() {
     const [show, setShow] = useState(false);
 
     useEffect(() => {
